8097200b5a19a99bf0cbed4f0305e7c17f5a5f0a
Rajveer(Chiranjeev)Shekhawat <121601167+rajvveer@users.noreply.github.com>
2024-01-02 12:21:18 +0530

    Update Header.js

--- src/components/GeneralScreens/Header.js
+++ src/components/GeneralScreens/Header.js
@@ -39,7 +39,7 @@ const Header = () => {
 
                 <Link to="/" className="logo">
                     <h5>
-                        MERN BLOG
+                        Hello BLOG
 
                     </h5>
                 </Link>
